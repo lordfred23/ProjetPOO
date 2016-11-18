178fa6c4090aea9ff6845070063c41572fb7cbb7
lordfred23 <fred.cote23@gmail.com>
2016-11-18 08:40:53 -0500

    .cs create

---
+++ JeuxVaisseaux/CControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuxVaisseaux
+{
+    class CControl
+    {
+        public CControl()
+        {
+            string test = "hello";
+        }
+    }
+}
